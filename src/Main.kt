5f09835fe4560428bf4c0a30aa9a62c87c48fb0d
Admin <reteamboss@gmail.com>
2024-09-23 15:56:45 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello World!")
+    var viktor = Person("Виктор")
+
+    viktor say "Добрый день"
+}
+
+class Person(var name:String){
+
+    infix fun say(message: String){
+        println("$name сказал $message")
+    }
 }
